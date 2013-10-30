825f29cc038170a41ee089ec4dc4c8ada129f6c3
Timo Stollenwerk <contact@timostollenwerk.net>
2013-10-30 10:05:54 +0100

    Add plone.performancetesting pkg.

---
+++ src/plone.performancetesting/src/plone/performancetesting/testing.py
@@ -0,0 +1,36 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+
+class PloneperformancetestingLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import plone.performancetesting
+        xmlconfig.file(
+            'configure.zcml',
+            plone.performancetesting,
+            context=configurationContext
+        )
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'plone.performancetesting:default')
+
+PLONE_PERFORMANCETESTING_FIXTURE = PloneperformancetestingLayer()
+PLONE_PERFORMANCETESTING_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONE_PERFORMANCETESTING_FIXTURE,),
+    name="PloneperformancetestingLayer:Integration"
+)
+PLONE_PERFORMANCETESTING_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PLONE_PERFORMANCETESTING_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="PloneperformancetestingLayer:Functional"
+)
